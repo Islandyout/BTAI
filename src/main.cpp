ca12bdaa598e61121ee7738a16aa4af9871cd515
Rastacia Fowlin <108405450+Islandyout@users.noreply.github.com>
2026-09-12 00:04:52 -0500

    Allow opening BTAI projects

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "btai/core/Engine.hpp"
+#include <filesystem>
 
-int main() {
-  btai::Engine engine;
+int main(int argc,char** argv) {
+  btai::EngineConfig config;
+  if(argc>2)return 2;
+  if(argc==2)config.projectManifest=std::filesystem::path(argv[1]);
+  btai::Engine engine(config);
   return engine.run();
 }
